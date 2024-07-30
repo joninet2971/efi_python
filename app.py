02570bbb22afe74a01a7c2c8abe32251c1f7a10b
RodoGitHub <r.palacios@itecriocuarto.org.ar>
2024-07-30 15:14:19 -0300

    import alchemy

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import requests
 
 from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/equipos_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
 
 @app.route("/")
 def index():
